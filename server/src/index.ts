0d5f73c0d1e499c48ec8ec1f7e5cac29b74e0edf
akshatverma1234 <131528804+akshatverma1234@users.noreply.github.com>
2026-02-01 20:48:49 +0530

    Change MongoDB URI to perkstack-platform

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
-  process.env.MONGODB_URI || "mongodb://localhost:27017/benefits-platform";
+  process.env.MONGODB_URI || "mongodb://localhost:27017/perkstack-platform";
 
 app.use(cors());
 app.use(express.json());
